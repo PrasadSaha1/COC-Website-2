9f26dc119497e9a63eee8a589488e573652b1c75
PrasadSaha1 <prasadsaha11@gmail.com>
2024-12-25 13:50:37 -0500

    Pre-Alpha #3

--- COC_app/COC_app/settings.py
+++ COC_app/COC_app/settings.py
@@ -22,10 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9yefx$wka!a52qcj@=3k13yr1*#_x)-@jrgoe88c(0)gt_m$n+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [".vercel.app"]
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app', 'COC_app.vercel.app']
 
 # Application definition
 
